fab6b5c3aa23ee5aa28a62630a48eddca1c234eb
Sam Henderson <shendo87@gmail.com>
2023-03-21 10:45:24 -0700

    Create very basic front end

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import 'vite/modulepreload-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -6,5 +7,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
