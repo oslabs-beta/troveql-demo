0fb2c60c27d94b801dc6bf4bacadf01820138f3a
erikahjung <erika.h.jung@gmail.com>
2023-03-21 11:28:11 -0700

    server starts

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 const PORT = 4000;
 
 // Clients can execute a query named movies and the server will return an array of zero or more Movies
+//will need to add Mutations whenever we're ready...
 const typeDefs = `#graphql
   # "Movie" is a type that lets clients query the API by movie title (required)
   type Movie {
@@ -13,7 +14,7 @@ const typeDefs = `#graphql
   # The "Query" type lists all the ways clients can query the GraphQL API and the return type for each
   # The "movie" query returns an array of zero or more Movie objects
   type Query {
-    movies: [Movie]
+    movies: [Movie!]!
   }
 `;
 
