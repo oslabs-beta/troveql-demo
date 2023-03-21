0309c55bea9b140ba58edef7d1c48f2cefa53f92
Tricia Yeh <triciayeh@Tricias-MacBook-Air.local>
2023-03-21 11:29:18 -0700

    created db schema, continuing to create dummy rows

--- db/movie_create.sql
+++ db/movie_create.sql
@@ -1,7 +1,7 @@
 -- create the tables
 -- default is the public schema 
 -- 
--- psql -d postgres://akzvayps:qTVWKmn-i8cBzmDagJB6tvGGWs02ApGn@mahmud.db.elephantsql.com/akzvayps -f user_create.sql
+-- psql -d postgres://akzvayps:qTVWKmn-i8cBzmDagJB6tvGGWs02ApGn@mahmud.db.elephantsql.com/akzvayps -f movie_create.sql
 -- CREATE TABLE products ( ... ); === CREATE TABLE public.products ( ... );
 
 SET statement_timeout = 0;
@@ -18,60 +18,41 @@ SET row_security = off;
 -- query for public.user so not to grab current user from SQL
 
 
-CREATE TABLE IF NOT EXISTS public.users (
+CREATE TABLE IF NOT EXISTS public.movies (
 	"_id" serial NOT NULL,
-	"username" varchar NOT NULL UNIQUE,
-	"password" varchar NOT NULL,
-	"first_name" varchar NOT NULL,
-	"last_name" varchar NOT NULL,
+	"movie_id" varchar NOT NULL UNIQUE,
+	"original_title" varchar NOT NULL,
+	"original_language" varchar NOT NULL,
+	"popularity" varchar,
+	"vote_count" bigint,
 	CONSTRAINT "users_pk" PRIMARY KEY ("_id")
 ) WITH (
   OIDS=FALSE
 );
 
-CREATE TABLE IF NOT EXISTS public.boards (
+CREATE TABLE IF NOT EXISTS public.popularMovies (
 	"_id" serial NOT NULL,
-	"name" varchar NOT NULL,
-	CONSTRAINT "boards_pk" PRIMARY KEY ("_id")
+	"movie_id" varchar NOT NULL UNIQUE,
+	CONSTRAINT "popularMovies_pk" PRIMARY KEY ("_id")
 ) WITH (
   OIDS=FALSE
 );
 
-CREATE TABLE IF NOT EXISTS public.columns (
+CREATE TABLE IF NOT EXISTS public.reviews (
 	"_id" serial NOT NULL,
-	"name" varchar NOT NULL,
-  "board_id" bigint NOT NULL,
-  CONSTRAINT "columns_pk" PRIMARY KEY ("_id")
+	"author" varchar NOT NULL,
+  "content" varchar NOT NULL,
+	"created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	"updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	"movie_id" varchar NOT NULL UNIQUE,
+  CONSTRAINT "reviews_pk" PRIMARY KEY ("_id")
  ) WITH (
     OIDS=FALSE
 );
 
 
-
-CREATE TABLE IF NOT EXISTS public.cards (
-	"_id" serial NOT NULL,
-	"task" varchar NOT NULL,
-	"column_id" bigint NOT NULL,
-	CONSTRAINT "cards_pk" PRIMARY KEY ("_id")
-) WITH (
-		OIDS=FALSE
-);
-
-
--- join table 
-CREATE TABLE IF NOT EXISTS public.users_boards (
-  "_id" serial NOT NULL,
-  "user_id" bigint NOT NULL,
-  "board_id" bigint NOT NULL,
-  CONSTRAINT "users_boards_pk" PRIMARY KEY ("_id")
-) WITH (
-  OIDS=FALSE
-);
-
 -- to add the foreign keys at the end of the table creation
 -- add foreign key to users_itineraries called "users_itineraries_fk"
 -- ALTER TABLE public.boards ADD CONSTRAINT "users_itineraries_fk0" FOREIGN KEY ("user_id") REFERENCES  public.users("_id");
-ALTER TABLE public.users_boards ADD CONSTRAINT "user_boards_fk0" FOREIGN KEY ("user_id") REFERENCES public.users("_id");
-ALTER TABLE public.users_boards ADD CONSTRAINT "user_boards_fk1" FOREIGN KEY ("board_id") REFERENCES public.boards("_id");
-ALTER TABLE public.columns ADD CONSTRAINT "columns_boards_fk0" FOREIGN KEY ("board_id") REFERENCES public.boards("_id");
-ALTER TABLE public.cards ADD CONSTRAINT "cards_fk0" FOREIGN KEY ("column_id") REFERENCES public.columns("_id");
+ALTER TABLE public.popularMovies ADD CONSTRAINT "movie_id_fk0" FOREIGN KEY ("movie_id") REFERENCES public.movies("_id");
+ALTER TABLE public.reviews ADD CONSTRAINT "movie_id_fk1" FOREIGN KEY ("movie_id") REFERENCES public.movies("_id");
