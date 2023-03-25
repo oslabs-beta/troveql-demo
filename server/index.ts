9f47da95c1e6d2f481dfbb10769b94647295578c
Tricia Yeh <triciayeh@Tricias-MacBook-Air.local>
2023-03-25 12:33:46 -0700

    added ts file > > Co-authored-by: triciacorwin <triciacorwin@gmail.com> Co-authored-by: samhhenderson <shendo87@gmail.com> Co-authored-by: Alex T. Klein <a-t-klein@users.noreply.github.com> Co-authored-by: erikahjung <erika.h.jung@gmail.com>

---
+++ server/index.ts
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const { graphqlHTTP } = require("express-graphql");
+const cors = require('cors');
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// The TroveQLCache middleware function requires 2 arguments:
+// (1) persistence value for the cache
+// (2) your server's graphQL URL endpoint
+const { TroveQLCache } = require('troveql');
+const cache = new TroveQLCache(3000, 'http://localhost:4000/graphql');
+app.use('/troveql', 
+  cache.queryCache,
+  (req, res) => res.status(200).json(res.locals.value)
+);
+
+const { schema } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+app.use('/graphql', 
+  graphqlHTTP({
+    schema: schema, 
+    rootValue: resolvers,
+    // context: {},
+    graphiql: true
+  })
+);
+
+app.listen(PORT, () => console.log(`Express Server ready at http://localhost:${PORT}`));
