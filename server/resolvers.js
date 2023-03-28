23bcfb16839cf5b9a03fa1df5dcdb66869f5b666
erikahjung <erika.h.jung@gmail.com>
2023-03-27 21:04:10 -0700

    updated graphQL server schema with @graphql-tools/schema package and also minor changes to resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,25 +2,27 @@ const { getMovies, getMovie, getActors } = require('./data');
 
 // Resolvers describe what the server returns to the client for a specific query
 const resolvers = {
-  // Query: {
-      movies: () => getMovies(),
-      movie: async (obj, { id }, context, info) => {
+  Query: {
+      movies: () => {
+        console.log('inside movie(s) query');
+        return getMovies();
+      },
+      movie: async (obj, args, context, info) => {
         console.log('inside movie query');
-        // console.log('args', args);
-        const result = await getMovie(3);
+        console.log('args', args);
+        const result = await getMovie(args.id);
         console.log('result', result);
         return result;
-      },
-      actors: () => getActors(),
-  // },
-  // Movie: {
-  //   actors: async (obj) => {
-  //     console.log('inside movie and actors query');
-  //     const result = await getActors(obj.id);
-  //     // console.log('result', result);
-  //     return result;
-  //   },
-  // }
+      }
+  },
+  Movie: {
+    actors: async ({ id }) => {
+      console.log('inside movie and actors query');
+      const result = await getActors(Number(id));
+      // console.log('result', result);
+      return result;
+    },
+  }
 };
 
 module.exports = { resolvers };
--- server/schema.js
+++ server/schema.js
@@ -1,11 +1,12 @@
-const { buildSchema } = require("graphql");
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { resolvers } = require('./resolvers');
 
 // Clients can execute a query named movies and the server will return an array of zero or more Movies
 //will need to add Mutations whenever we're ready...
 const typeDefs = `#graphql
   # "Movie" is a type that lets clients query the API by movie title (required)
   type Movie {
-    id: ID
+    id: Int
     title: String!
     genre: String
     year: Int
@@ -13,22 +14,24 @@ const typeDefs = `#graphql
   }
 
   type Actor {
-    id: ID
+    id: Int
     name: String!
     gender: String
     place_of_birth: String
+    movies: [Movie]
+    movie_id: Int
   }
 
   # The "Query" type lists all the ways clients can query the GraphQL API and the return type for each
   # The "movie" query returns an array of zero or more Movie objects
   type Query {
-    movies: [Movie]
-    movie(id: ID!): Movie
-    actors: [Actor]
-    actor(id: ID!): Actor
+    movies: [Movie!]!
+    movie(id: Int): Movie!
+    actors: [Actor!]!
+    actor(id: Int): Actor!
   }
 `;
 
-const schema = buildSchema(typeDefs);
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 module.exports = { schema };
